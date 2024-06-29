1071546eac410a7248a97f351618f4bdf35e8d9a
st sama <albertezoula98@gmail.com>
2024-06-29 12:48:48 +0000

    implemantation of the database, a user collection and authentication with google successfully done

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 require('dotenv').config();
 
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts'); 
+const connectDB = require('./server/config/db')
+const session = require('express-session'); // remenber a user when he's connected
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
+
+
 
 // server and port initialisation and declaration
 const app = express();
 const port = 5000 || process.env.PORT
 
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    // cookie: {maxAge: new Date(Date.now() + (3600000))} (cookie expire on 1 hour)
+    // ----Date.now() - 30 * 24 * 60 * 1000  (for a month) -----
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // middleware that's going to allow us transfer data from page to page
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//connect to Database
+connectDB();
 
 //static files
 app.use(express.static('public'));
@@ -21,6 +47,7 @@ app.set('view engine', 'ejs');
 
 //Routes
 app.use('/', require('./server/routes/index'));
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/dashboard'));
 
 
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require('../middleware/checkAuth')
 const dashboardController = require('../controllers/dashboardController');
 /**
  * App Routes
  */
-router.get('/dashboard', dashboardController.dashboard);
+router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
 
 
 
