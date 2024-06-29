a2835c420684931cf8d7131b2a4035593a888b25
st sama <albertezoula98@gmail.com>
2024-06-29 22:44:52 +0000

    creation of the note collection and displaying notes on the dashboard

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,14 +1,24 @@
+const Note = require("../models/note");
+const mongoose = require("mongoose");
+
 /**
  * GET
  * Dashboard
  */
 exports.dashboard = async (req, res) => {
-    const locals = {
-      title: "NodeJs Notes",
-      description: "Free NodeJs Notes App",
-    };
-    res.render("dashboard/index", {
-        locals,
-        layout: '../views/layouts/dashboard'
-      });
+  const locals = {
+    title: "NodeJs Notes",
+    description: "Free NodeJs Notes App",
   };
+  try {
+    const notes = await Note.find({});
+    res.render("dashboard/index", {
+      username: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
